7b14f9578c8852f57fef8d5c1ebcfc8a3fc7c662
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-06 10:58:26 -0500

    Named routes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/welcome_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -15,7 +18,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        'login': (context) => LoginScreen(),
+        'registration': (context) => RegistrationScreen(),
+        'chat': (context) => ChatScreen(),
+      }
     );
   }
 }
