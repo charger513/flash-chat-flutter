7bc750e996806d80b51bb4fa377ef6d25467eed0
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-06 11:21:56 -0500

    Refactor routes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/welcome_screen.dart';
 
@@ -21,9 +24,9 @@ class FlashChat extends StatelessWidget {
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
-        'login': (context) => LoginScreen(),
-        'registration': (context) => RegistrationScreen(),
-        'chat': (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       }
     );
   }
